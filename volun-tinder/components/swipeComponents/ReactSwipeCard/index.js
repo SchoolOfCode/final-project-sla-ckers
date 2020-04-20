0f8643766c65eceb8279e037785df0e6a5e08e08
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-04-20 16:48:47 +0100

    swipe component set up to take info from our sample data, and basic styling

--- volun-tinder/components/swipeComponents/ReactSwipeCard/index.js
+++ volun-tinder/components/swipeComponents/ReactSwipeCard/index.js
@@ -3,12 +3,13 @@
 //SETUP AND TEST:
 //TODO: Install/import TinderCard and set up code ✅ 
 //TODO: Test out component with dummyOrgs data array so that the names come through and the swiping works ✅ 
-//TODO: Pull the test images through into the test component to make sure we know how to make images display
 
 //MAKE INTO USABLE COMPONENT WITH OUR PROPER TEST DATA:
-//TODO: Set up a prop to take in the sampleOrgProfs array of data for the relevant category (will be passed in where the component is called), and change const orgs to equal that array 
+//TODO: Set up a prop to take in the sampleOrgProfs array of data for the relevant category (will be passed in where the component is called), and change const orgs to equal that array ✅ 
+//TODO: Have a category prop to feed into header/description ✅  
 //TODO: Design card to pull out and display each category of info for each org individually for swiping 
 //TODO: Set up state (or reducer??) to record orgs that have been swiped right on (with function to do this on the right direction's swipe) - **NOTE: This may or may not need to be on this level or higher up... We'll see how it goes.**
+//TODO: Need a way to progress to the match page (button or triggered on last swipe...?)
 
 --------------------------------------------------------------------------*/
 
@@ -16,15 +17,14 @@ import React, { useState } from 'react';
 import TinderCard from 'react-tinder-card';
 import css from './reactSwipeCard.module.css';
 
-const dummyOrgs = [
-  { name: 'Pistols for Pandas', url: '../sampleImgs/sample-img-panda.jpg' },
-  { name: 'Hedgehogs', url: '../sampleImgs/sample-img-swan.jpg' },
-  { name: 'Swans', url: '../sampleImgs/sample-img-hedgie.jpg' },
-];
+// const dummyOrgs = [
+//   { name: 'Pistols for Pandas', url: '../sampleImgs/sample-img-panda.jpg' },
+//   { name: 'Hedgehogs', url: '../sampleImgs/sample-img-swan.jpg' },
+//   { name: 'Swans', url: '../sampleImgs/sample-img-hedgie.jpg' },
+// ];
 
-function ReactSwipeCard() {
+function ReactSwipeCard({ category, orgs }) {
   const [lastDirection, setLastDirection] = useState();
-  const orgs = dummyOrgs;
 
   function swiped(direction, nameToDelete) {
     console.log('removing: ' + nameToDelete);
@@ -34,35 +34,65 @@ function ReactSwipeCard() {
   function outOfFrame(name) {
     //We can have a version of this function for swiping right and add the charity to the list that we display at the end
     //can take in the direction (along with name) and then if direction === right, add to a state or reducer
+    //This needs to happen at a level above this component I think...
     console.log(name + ' left the screen!');
   }
 
   return (
-    <div className={css.swipeCard}>
-      <h1>Meet or Yeet!</h1>
-      <h2>Yeet Some Charities</h2>
-      <div className="cardContainer">
+    <div className={css.swipeInterface}>
+      {/* Title and intro: */}
+      <h1>Organisations About {category} Who'd Love to Meet You</h1>
+      <h2>
+        Here are some organisations looking for a volunteer passionate about{' '}
+        {category.toLowerCase()} too. If you like how they sound, swipe right to
+        add them to your matches list. If they aren't for you, no problem. Swipe
+        left to send them on their way.
+      </h2>
+
+      {/* Message that renders based on swipe direction: */}
+      {lastDirection ? (
+        lastDirection === 'right' ? (
+          <h2 className={css.infoText}>
+            It's a match! We've added this organisation to your list.
+          </h2>
+        ) : (
+          <h2 className={css.infoText}>
+            Not a match? That's alright. See if the next one strikes your fancy!
+          </h2>
+        )
+      ) : (
+        <h2 className={css.infoText}>Get swiping!</h2>
+      )}
+
+      {/* Renders the cards: */}
+      <div className={css.cardContainer}>
         {orgs.map((org) => (
           <TinderCard
-            className="swipe"
+            className={css.card}
             key={org.name}
             onSwipe={(dir) => swiped(dir, org.name)}
             onCardLeftScreen={() => outOfFrame(org.name)}
           >
-            <div
-              style={{ backgroundImage: 'url(' + org.url + ')' }}
-              className="card"
-            >
-              <h3>{org.name}</h3>
+            <div style={{ backgroundImage: 'url(' + org.url + ')' }}>
+              <h3>Hi, we're {org.orgName}!</h3>
+              <p>{org.briefBio}</p>
+              <p>
+                Is it us you're looking for? Swipe right if this sounds like
+                something you'd enjoy:
+              </p>
+              <p>{org.opportunities[0].oppDescrip}</p>
+
+              <p>Time requirement: {org.opportunities[0].timeReq}</p>
+              <p>What we're looking for in a match:</p>
+              <ul className={css.qualitiesList}>
+                {org.threeThingsVolsCantDoWithout.map((thing) => (
+                  <li>{thing}</li>
+                ))}
+              </ul>
             </div>
           </TinderCard>
         ))}
       </div>
-      {lastDirection ? (
-        <h2 className="infoText">You yeeted it {lastDirection}</h2>
-      ) : (
-        <h2 className="infoText" />
-      )}
     </div>
   );
 }
--- volun-tinder/libs/sampleOrgProfs.js
+++ volun-tinder/libs/sampleOrgProfs.js
@@ -1,26 +1,55 @@
-export const sampleAnimalOrgs = [
-  {
-    orgName: '',
-    briefBio: '',
-    opportunities: [{ oppDescrip: '', timeReq: '' }],
-    threeThingsVolsCantDoWithout: ['', '', ''],
-  },
-  {
-    orgName: '',
-    briefBio: '',
-    opportunities: [{ oppDescrip: '', timeReq: '' }],
-    threeThingsVolsCantDoWithout: ['', '', ''],
-  },
-  {
-    orgName: '',
-    briefBio: '',
-    opportunities: [{ oppDescrip: '', timeReq: '' }],
-    threeThingsVolsCantDoWithout: ['', '', ''],
-  },
-];
-
 // Organisation name
 // Extremely brief bio of what they're about/what their purpose/mission is - like our "ten second intro"
 // Volunteer opportunities and expectations (list with a very short description for each type of volunteer opportunity along with very clear expectations section incl. time commitment and any other essential requirements i.e. DBS check)
 // Something fun and dating-app-y to get the vibe of the org... OKCupid had the "What three things could you not live without" - something similar to this perhaps?
 // Contact info for potential volunteers to get in touch (display during the shortlist)
+
+export const sampleAnimalOrgs = [
+  {
+    orgName: 'Pistols for Pandas',
+    briefBio:
+      'A charity aiming to help pandas protect themselves from extinction',
+    opportunities: [
+      {
+        oppDescrip:
+          'Remote opportunity to raise the organisational profile through our social media channels',
+        timeReq: '2 hrs a week',
+      },
+    ],
+    threeThingsVolsCantDoWithout: ['a way with words', 'charm', 'wit'],
+  },
+  {
+    orgName: 'Hungry Hedgehogs',
+    briefBio:
+      "The way to a hedgehog's heart is through its stomach. Show some love for your prickly friends and put your culinary skills to the test.",
+    opportunities: [
+      {
+        oppDescrip:
+          'Cook up some delish delights for our endangered nocturnal garden vistiors',
+        timeReq: '4 hrs a week',
+      },
+    ],
+    threeThingsVolsCantDoWithout: [
+      'culinary skills',
+      'A big heart',
+      'A love of the great outdoors',
+    ],
+  },
+  {
+    orgName: 'Forever Swan',
+    briefBio:
+      'Swans mate for life.  Help the singletons find their true match by creating some ambient surroundings',
+    opportunities: [
+      {
+        oppDescrip:
+          'Physical work.  Clean, tidy and prune your local pond or lake.  Prepared to get dirty.',
+        timeReq: '3 hrs a week',
+      },
+    ],
+    threeThingsVolsCantDoWithout: [
+      'physically fit',
+      'proactive',
+      'considerate',
+    ],
+  },
+];
