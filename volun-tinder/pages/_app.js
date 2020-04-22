2d54dee43d53d57511f9be5f77be6c436b54e9c5
leckzilla <leckier@hotmail.com>
2020-04-22 15:57:22 +0100

    cracked global css bizniz

---
+++ volun-tinder/pages/_app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/global.css";
+import QuizApp from "../components/quizComponents/QuizApp/index";
+
+//Need to build a layout for this page?
+//Header
+//Welcome message and brief explanation of what Volun-Tinder is all about
+//Link/button to take to the quiz
+//Link to knowledge base
+//Directory of orgs?? - Stretch
+
+export default function Home() {
+  return (
+    <div>
+      <QuizApp />
+    </div>
+  );
+}
