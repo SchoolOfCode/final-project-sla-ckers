b8b0fbf3f8db0503b3bbffb1c1ff9e7106230656
leckzilla <leckier@hotmail.com>
2020-04-27 15:41:51 +0100

    working manifest

--- volun-tinder/components/Header.js
+++ volun-tinder/components/Header.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
 
 function Header() {
   return (
     <div id="header">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>Volun-Tinder</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="../public/heart_hands.png"
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+        />
+        <link
+          href="../public/heart_hands192.png"
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+        />
+        <link rel="icon" href="icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
+
       {/* <img alt="logo" /> */}
       <h1>Volun-Tinder</h1>
       <div id="navLinks">
