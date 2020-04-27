094a4c52d8d74e88825329e2abadfc41b1991ea0
AdoptedThistle <sbarkle.1@googlemail.com>
2020-04-27 14:01:23 +0100

    third commit. All Volunteer data fields added

--- volun-tinder/pages/vol-registration.js
+++ volun-tinder/pages/vol-registration.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //Create a basic form for volunteers to log their details following the swipe component.
 //Details provided by the volunteer will be used to populate individual profiles/cards
-//TODO: Create a form that captures the following data:  firstName, lastName, location, charityCategory, hrCommitment, tasks, bio.
+//TODO: Create a form that captures the following data:  firstName, lastName, location, charityType, hrCommitment, bio.
 //TODO: Link to a profile picture
 //TODO: Button that onClick saves the data in a JSON file
 
@@ -35,6 +35,33 @@ function VolunteerRegistration({ takeInData }, { volunteerData }) {
           onChange={takeInData}
           value={volunteerData.location}
           name="location"
+        ></input>
+
+        <label for="charityType">Charity Type:</label>
+        <input
+          type="text"
+          id="location"
+          onChange={takeInData}
+          value={volunteerData.charityType}
+          name="charityType"
+        ></input>
+
+        <label for="hours">Hours available a week:</label>
+        <input
+          type="text"
+          id="hours"
+          onChange={takeInData}
+          value={volunteerData.hours}
+          name="hours"
+        ></input>
+
+        <label for="bio">Volunteer Bio:</label>
+        <input
+          type="text"
+          id="bio"
+          onChange={takeInData}
+          value={volunteerData.bio}
+          name="bio"
         ></input>
       </form>
     </div>
