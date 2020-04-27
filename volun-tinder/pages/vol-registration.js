c966546290a31ebc0957b404be64fdadf4b3ff3d
AdoptedThistle <sbarkle.1@googlemail.com>
2020-04-27 13:50:21 +0100

    second commit. Started form structure.

--- volun-tinder/pages/vol-registration.js
+++ volun-tinder/pages/vol-registration.js
@@ -5,4 +5,38 @@ import React from "react";
 //TODO: Create a form that captures the following data:  firstName, lastName, location, charityCategory, hrCommitment, tasks, bio.
 //TODO: Link to a profile picture
 //TODO: Button that onClick saves the data in a JSON file
-//
+
+function VolunteerRegistration({ takeInData }, { volunteerData }) {
+  return (
+    <div>
+      <form className="volunteerForm">
+        <label for="contact-fname">First Name:</label>
+        <input
+          type="text"
+          id="contact-fname"
+          onChange={takeInData}
+          value={volunteerData.firstName}
+          name="firstName"
+        ></input>
+
+        <label for="contact-lname">Last Name:</label>
+        <input
+          type="text"
+          id="contact-lname"
+          onChange={takeInData}
+          value={volunteerData.lastName}
+          name="lastName"
+        ></input>
+
+        <label for="location">Location</label>
+        <input
+          type="text"
+          id="location"
+          onChange={takeInData}
+          value={volunteerData.location}
+          name="location"
+        ></input>
+      </form>
+    </div>
+  );
+}
