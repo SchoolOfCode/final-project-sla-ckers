b408dc98cddcaf1fffea68c8a8d8335d6afcfb04
AdoptedThistle <sbarkle.1@googlemail.com>
2020-04-27 12:00:17 +0100

    first commit: plan for volunteer registration form

---
+++ volun-tinder/pages/vol-registration.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//Create a basic form for volunteers to log their details following the swipe component.
+//Details provided by the volunteer will be used to populate individual profiles/cards
+//TODO: Create a form that captures the following data:  firstName, lastName, location, charityCategory, hrCommitment, tasks, bio.
+//TODO: Link to a profile picture
+//TODO: Button that onClick saves the data in a JSON file
+//
