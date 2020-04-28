eae5ca375e7d35b7edcef8a01bd1c4393a8f36d1
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-04-28 11:04:43 +0100

    first test actually working omgz

--- volun-tinder/components/swipeComponents/MatchApp/matchApp.test.js
+++ volun-tinder/components/swipeComponents/MatchApp/matchApp.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { matchReducer } from './index';
 
 describe('reducer', () => {
-  it('should add a todo', function () {
-    const initialMatchState = { matchResults: [] };
+  it('should add test org to matchResults array', function () {
+    const initialMatchState = { matchResults: [], swipeRights: 0 };
     const org = { orgName: 'Test Org' };
     const actual = matchReducer(initialMatchState, {
       type: 'swipe-right',
       payload: org,
     });
-    expect(actual).toEqual([{ org }]);
+    expect(actual).toEqual({ matchResults: [org], swipeRights: 1 });
   });
 });
