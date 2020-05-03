a46c4cdea14e934e6154a11a3a224d027c5bbb91
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-03 23:17:45 +0100

    added in Firebase code and noted what each bit does. as yet untested though.

--- volun-tinder/components/AuthComponents/Login/index.js
+++ volun-tinder/components/AuthComponents/Login/index.js
@@ -3,19 +3,27 @@
 //Basic login form (and google option) modelled off of what we did during the Firebase lesson
 //I've set up my Firebase account with the project as well, so it's ready to receive users once we hook things up
 
-//My code I'm using as an example to shape this from: https://github.com/lizkaufman/first-firebase-auth-app
-
-//TODO: Define loggedInUser logic and pass to this component as props
-//TODO: Add email/password fields on org form and the logic that goes with them to register the user's account (perhaps a section of the form that also has an update with the google option?)
-//TODO: Have an option that an org can choose from on the page (i.e. buttons) - if they're new, it takes them to the full form, and if they're returning, it shows them this login form instead
+//TODO: Define loggedInUser logic at page level and pass to this component as props
+//TODO: Add email/password fields on org form and the logic that goes with them to register the user's account (and google!)
+//TODO: Have both login and register buttons - depends on if it's their first visit or logging in again
 //TODO: Make sure that once the user is logged in, this form is conditionally rendered to no longer show (based on the loggedInUser state)
 
 //---------------------------
 
-import React, { useState } from 'react';
+import React from 'react';
 
-function Login({ handleSubmit, handleChange, handleSignInWithPopup }) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+function Login({
+  setFormState,
+  formState,
+  handleSubmit,
+  handleSignInWithPopup,
+  handleSignup,
+}) {
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormState({ ...formState, [name]: value });
+  }
 
   return (
     <div>
@@ -42,6 +50,7 @@ function Login({ handleSubmit, handleChange, handleSignInWithPopup }) {
       <button onClick={handleSignInWithPopup}>
         Sign in with your Google account
       </button>
+      <button onClick={handleSignup}>Sign up as new user</button>
     </div>
   );
 }
--- volun-tinder/pages/org-form.js
+++ volun-tinder/pages/org-form.js
@@ -1,7 +1,7 @@
 /* ----------------------------------------------------------------------------
 PLAN FOR LOGIN:
 
-- Need logic to listen for auth state change (via Firebase) and set the loggedInUser state accordingly - presumably on this level, so we can conditionally render the org form and the logout button once the user is logged in -> as set up but commented out for now below
+- Need logic to listen for auth state change (via Firebase) and set the loggedInUser state accordingly - presumably on this level, so we can conditionally render the org form and the logout button once the user is logged in -> as set up but commented out for now below.
 
 - Initial flow:
   1. User goes to org dashboard -> 
@@ -19,15 +19,119 @@ import Layout from '../components/Layout';
 import Login from '../components/AuthComponents/Login/index';
 import LogoutButton from '../components/AuthComponents/LogoutButton/index';
 
-export default function orgForm() {
+import firebase from 'firebase';
+
+export default function OrgForm() {
+  //state for the logged in user; this way it'll trigger a re-render and store the user object in here:
   const [loggedInUser, setLoggedInUser] = useState(null);
+  //form state that holds the info from the login and/or register form
+  const [formState, setFormState] = useState({ email: '', password: '' });
+
+  //Observer that watches out for change of auth state (i.e. a login):
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // If there's a user object, user is signed in
+      let email = user.email;
+      console.log(`${email} is logged in`);
+    } else {
+      // If there's no user object, user is signed out
+      console.log('no user signed in');
+    }
+    //Sets the loggedInUser state with the user object:
+    setLoggedInUser(user);
+    console.log(user);
+  });
+
+  //Function hooked up to signup button in login form
+  function handleSignup() {
+    //Gets the details from the form:
+    const email = formState.email;
+    const password = formState.password;
+    //TODO: FIXME:Remove this console.log once tested!
+    console.log({ email, password });
+    //Tells Firebase to make the new user:
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function (error) {
+        console.error(error);
+      });
+    //Clears the form after it sends the details to Firebase:
+    setFormState({ email: '', password: '' });
+    console.log(`new user signed up with email ${email}`);
+  }
+
+  //Logs in an existing user:
+  function handleSubmit(event) {
+    event.preventDefault();
+    //gets info from login form:
+    const email = formState.email;
+    const password = formState.password;
+    //TODO: FIXME:Remove this console.log once tested!
+    console.log({ email, password });
+    //passes it to Firebase and tells it to sign the user in with these credentials:
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(function (error) {
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        //handles wrong password with specific alert to user:
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password.');
+          //handles other errors with more general alert:
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+      });
+    //clears out the form once it's done:
+    setFormState({ email: '', password: '' });
+    console.log('login finished');
+  }
+
+  //Signs out signed-in user:
+  function handleSignout() {
+    //Does what it says on the tin! Tells Firebase to log 'em out:
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        console.log('user logged out');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  //Calls on a new instance of Google's sign-in doohickey:
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+  //Sign in with google:
+  function handleSignInWithPopup() {
+    //Tells Firebase to open Google popup using the instance called on above, then the popup takes it from there:
+    firebase
+      .auth()
+      .signInWithPopup(googleProvider)
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
 
   return (
     <div>
       <Layout>
-        {/* {!loggedInUser && <Login /> }*/}
-        {/* {loggedInUser && <LogoutButton/>}
-        {loggedInUser && <InputComponent/>} */}
+        {!loggedInUser && (
+          <Login
+            formState={formState}
+            setFormState={setFormState}
+            handleSubmit={handleSubmit}
+            handleSignInWithPopup={handleSignInWithPopup}
+            handleSignup={handleSignup}
+          />
+        )}
+        {loggedInUser && <LogoutButton handleSignout={handleSignout} />}
+        {loggedInUser && <InputComponent />}
         <InputComponent />
       </Layout>
     </div>
