4cc4ed403aef2033a11c8220d1a3fb2ed7215d98
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-09 22:17:29 +0100

    intro text above swipe cards now responsive. swipe cards themselves wont respond to media query. trying to figure out why.

--- volun-tinder/public/sw.js
+++ volun-tinder/public/sw.js
@@ -124,10 +124,10 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "82dca635a629d8ab38c3ad85b2ad65a2"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "6af0f553cf1d2bce421f6cc5be89b8ad"
+    "revision": "4e83289d5b3f9b25b39d2d34bfe7b613"
   }, {
     "url": "/_next/static/runtime/webpack.js.map",
-    "revision": "c8b1e10fcde73990c620a8144b1219f3"
+    "revision": "d9208186bbda91b27d4c1c50cc927246"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
